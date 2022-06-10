ad10a7945310d6bde01cc7561a4118809829f239
Yumi_Amahane <51439946+Yumi-Amahane@users.noreply.github.com>
2022-06-10 23:57:54 +0900

    Create routing.js

---
+++ routing.js
@@ -0,0 +1,11 @@
+contents={
+  TITLE: "Rufina-Web-top",
+  HEADER: "HEADER",
+  CONTENTS: "テスト用コンテンツ",
+};
+
+function rendering(){
+  for (let key in contents){
+    document.getElementById(key)=contents[key];
+  }
+}
