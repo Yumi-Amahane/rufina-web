b2a793449058c9ea793957b323208cff0d1ba7c1
Yumi_Amahane <51439946+Yumi-Amahane@users.noreply.github.com>
2022-06-11 00:17:38 +0900

    Update routing.js

--- routing.js
+++ routing.js
@@ -1,13 +1,14 @@
 contents={
   TITLE: "Rufina-Web-top",
   HEADER: "HEADER",
-  CONTENTS: "テスト用コンテンツ",
+  CONTENT: "テスト用コンテンツ",
 };
 
 function rendering(){
   for (let key in contents){
-    console.log(key)
-    console.log(contents[key])
+    console.log("Rendering")
+    //console.log(key)
+    //console.log(contents[key])
     document.getElementById(key).innerHTML=contents[key];
   }
 }
